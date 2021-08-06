e4ab95df2ca4a464de476a6bac796a1b89b84891
ANUKIM <87270619+ANUKIM@users.noreply.github.com>
2021-08-06 19:54:33 +0530

    Add files via upload

---
+++ pro-c16-ta-1-reference-code-main/pro-c16-ta-1-reference-code-main/box.js
@@ -0,0 +1,19 @@
+class student
+  {
+  
+    constructor(name,age,grade)
+    {
+      this.name = name
+      this.age = age
+      this.grade = grade
+     
+
+    }
+    show()
+    {
+      console.log(this.name+this.age+this.grade);
+    }
+    
+  }
+
+  
---
+++ pro-c16-ta-1-reference-code-main/pro-c16-ta-1-reference-code-main/sketch.js
@@ -0,0 +1,18 @@
+var student1;
+var student2
+function setup() 
+{
+  createCanvas(400, 400);
+  student1 = new student("tee",13,10);
+ student2 = new student("v",16,12);
+ student1.show();
+  student2.show();
+}
+
+function draw() 
+{
+  background(220);
+  
+  
+}
+
